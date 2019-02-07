40f981e820b771fca309557d5bb703e3a6038e6b
Thomas Svartbäck <thomsva@gmail.com>
2019-02-07 18:07:07 +0200

    4.5 valmis

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,10 +1,16 @@
 const dummy = (blogs) => (1)
 
 const totalLikes = (blogs) => {
-  return blogs.map(blog => blog.likes).reduce((prev, next) => prev + next, 0)
+  return blogs.map(blog => blog.likes).reduce((total, num) => total + num, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const maxLikes = blogs.reduce((max, blog) => blog.likes > max ? blog.likes : max, 0)
+  return blogs.find(blog => blog.likes === maxLikes)
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
