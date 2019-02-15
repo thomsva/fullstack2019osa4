f16cb88d233acf42ceca9402703eb131e7bb21f0
Thomas Svartbäck <thomsva@gmail.com>
2019-02-15 19:06:07 +0200

    4.17 valmis

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
   } catch (exception) {
     next(exception)
@@ -12,10 +13,15 @@ blogsRouter.get('/', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
 
-  const blog = new Blog(request.body)
+  var blog = new Blog(request.body)
+  const user = await User.findOne()  //finds random user
+  blog.user = user
+
   if (blog.likes == null) blog.likes = 0
   try {
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(200).json(savedBlog)
   } catch (exception) {
     next(exception)
