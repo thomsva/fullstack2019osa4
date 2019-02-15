b0c55dd0cc22eebcbbb669b347a3fa6482fdfb89
Thomas Svartbäck <thomsva@gmail.com>
2019-02-15 20:40:46 +0200

    4.19 valmis

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
@@ -12,13 +21,21 @@ blogsRouter.get('/', async (request, response, next) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-
+  console.log('hep')
+  const token = getTokenFrom(request)
+  console.log('token', token)
   var blog = new Blog(request.body)
-  const user = await User.findOne()  //finds random user
-  blog.user = user
-
-  if (blog.likes == null) blog.likes = 0
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    blog.user = user
+
+    if (blog.likes == null) blog.likes = 0
+
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
